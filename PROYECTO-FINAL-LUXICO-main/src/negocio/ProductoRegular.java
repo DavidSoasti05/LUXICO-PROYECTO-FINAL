31285919bde1ea8b0336d8ca427642cc02ab23b0
DavidSoasti05 <david.soasti@udla.edu.ec>
2026-01-20 14:05:43 -0500

    Remove Serializable from ProductoRegular class
    Removed Serializable interface from ProductoRegular class.

--- PROYECTO-FINAL-LUXICO-main/src/negocio/ProductoRegular.java
+++ PROYECTO-FINAL-LUXICO-main/src/negocio/ProductoRegular.java
@@ -1,9 +1,7 @@
 package negocio;
 
-import java.io.Serializable;
-
-public class ProductoRegular extends Producto implements Serializable {
-    private static final long serialVersionUID = 1L;
+public class ProductoRegular extends Producto  {
+    
     public ProductoRegular(String codigo, String modelo, String talla, double precio, int stock, int stockMin) {
         super(codigo, modelo, talla, precio, stock, stockMin);
     }
