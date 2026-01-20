49c49a0096d12872149992bc377a5fb0e5ce400e
DavidSoasti05 <david.soasti@udla.edu.ec>
2026-01-20 14:02:09 -0500

    Remove Serializable from Bodega class
    Removed Serializable implementation from Bodega class.

--- PROYECTO-FINAL-LUXICO-main/src/negocio/Bodega.java
+++ PROYECTO-FINAL-LUXICO-main/src/negocio/Bodega.java
@@ -1,9 +1,7 @@
 package negocio;
 
-import java.io.Serializable;
-
-public class Bodega extends Usuario implements Serializable {
-    private static final long serialVersionUID = 1L;
+public class Bodega extends Usuario  {
+    
     public Bodega(String usuario, String clave, String nombre) {
         super(usuario, clave, nombre);
     }
