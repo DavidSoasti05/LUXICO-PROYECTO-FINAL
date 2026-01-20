8b81f847e983d4492759e32d73cbfa6bf269bf15
DavidSoasti05 <david.soasti@udla.edu.ec>
2026-01-20 14:06:41 -0500

    Remove Serializable from Ventas class
    Removed Serializable interface from Ventas class.

--- PROYECTO-FINAL-LUXICO-main/src/negocio/Ventas.java
+++ PROYECTO-FINAL-LUXICO-main/src/negocio/Ventas.java
@@ -1,9 +1,7 @@
 package negocio;
 
-import java.io.Serializable;
-
-public class Ventas extends Usuario implements Serializable {
-    private static final long serialVersionUID = 1L;
+public class Ventas extends Usuario  {
+    
     public Ventas(String usuario, String clave, String nombre) {
         super(usuario, clave, nombre);
     }
