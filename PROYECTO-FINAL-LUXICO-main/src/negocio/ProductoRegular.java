cf308d3d9ce9b38d867b9bb4dcba0497cb86fa8b
DavidSoasti05 <david.soasti@udla.edu.ec>
2026-01-22 11:56:20 -0500

    Fix formatting and add newline at end of ProductoRegular.java

--- PROYECTO-FINAL-LUXICO-main/src/negocio/ProductoRegular.java
+++ PROYECTO-FINAL-LUXICO-main/src/negocio/ProductoRegular.java
@@ -1,7 +1,6 @@
 package negocio;
 
 public class ProductoRegular extends Producto  {
-    
     public ProductoRegular(String codigo, String modelo, String talla, double precio, int stock, int stockMin) {
         super(codigo, modelo, talla, precio, stock, stockMin);
     }
